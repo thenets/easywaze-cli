c582228f421a10cfd5812a71fa20eca675b5f0c3
Luiz Felipe F M Costa <phelip@thenets.org>
2018-07-13 05:41:52 -0300

    A Python example file

--- src/main.py
+++ src/main.py
@@ -1,6 +1,41 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, Table, Column, Integer, String, MetaData, ForeignKey
 
-# db = create_engine('mysql://root:root@db:8000/db')
+# SQLAlquemy tutorial at
+# http://docs.sqlalchemy.org/en/latest/core/tutorial.html
 
-engine = create_engine(
-      "mysql+pymysql://root:root@db/waze?host=db?port=3306")
+#### IMPORTANT
+# Create a database called "waze" before continue!
+
+# Create engine
+engine = create_engine("mysql+pymysql://root:root@db/waze?host=db?port=3306")
+
+# Try to create table if not exist
+metadata = MetaData()
+users = Table('users', metadata,
+    Column('id', Integer, primary_key=True),
+    Column('name', String(255)),
+    Column('fullname', String(255)),
+)
+addresses = Table('addresses', metadata,
+    Column('id', Integer, primary_key=True),
+    Column('user_id', None, ForeignKey('users.id')),
+    Column('email_address', String(255), nullable=False)
+)
+metadata.create_all(engine)
+
+# Create new query
+ins = users.insert().values(name='jack', fullname='Jack Jones')
+
+# DEBUG show the SQL query
+# print(str(ins))
+# print(str(ins.compile().params))
+
+# Create a connector
+conn = engine.connect()
+
+# Execute the insert query
+result = conn.execute(ins)
+
+# DEBUG read result
+print(result)
+print(result.inserted_primary_key)
